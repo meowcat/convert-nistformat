3ea67ea67d92d9ef1dbb80af97aba9edbbb942d4
Michele Stravs <stravsmi@eawag.ch>
2023-10-12 08:23:26 +0200

    modular editor

--- editor_module.R
+++ editor_module.R
@@ -13,7 +13,7 @@ editor_module_ui <- function(id) {
                     multiple=FALSE, 
                     buttonType="default", 
                     startRoot=c(input=".")),
-    textOutput("filename"),
+    textOutput(NS(id, "filename")),
     aceEditor(NS(id, "editor"), mode = "yaml")
     )
 }
@@ -32,18 +32,37 @@ editor_module_server <- function(id) {
           roots = c(input="."), 
           input$choose)$datapath[1]
       )
+      message(fileName())
     })
     # show filename in ui
-    output$filename <- filename()
+    output$filename <- renderText({ fileName() })
     
     # load content when file is chosen
     observeEvent(fileName(), {
-      message("save")
       req(fileName())  # Ensure filename exists
-      message(fileName())
       content <- paste(readLines(fileName(), warn = FALSE), collapse = "\n")
       updateAceEditor(session, "editor", value = content)
     })
+    
+    # overwrite file on "Save"
+    observeEvent(input$save, {
+      req(fileName())  # Ensure filename exists
+      content <- input$editor
+      write_lines(content, fileName())
+    })
+    
+    # on "Save as", write to new file and change file name
+    observeEvent(input$saveas, {
+      if(is.integer(input$saveas))
+        return()
+      fileName_ <- parseSavePath(
+        roots = c(input="."), 
+        input$saveas)$datapath[1]
+      content <- input$editor
+      write_lines(content, fileName_)
+      fileName(fileName_)
+    })
+    
     
     
   })
