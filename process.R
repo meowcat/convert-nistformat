e40d8be37e5d30ad964a6f859b251d01265fedc8
Michele Stravs <stravsmi@eawag.ch>
2024-04-08 15:30:52 +0200

    Improved mzMine processing

--- process.R
+++ process.R
@@ -123,11 +123,17 @@ iwalk(
     converting_map <- converting_read %>%
       SpectraMapping:::mapVariables(mapping)
     
+    fs::dir_create(fs::path(cache_folder, "mapped"))
+    save(converting_map, file=fs::path(cache_folder, "mapped", glue("{i}.RData")))
+    
     # message("Available spectra variables:")
     # str(spectraVariables(converting_map))
     
     message("Applying extra processing steps")
     converting_process <- process_spectra_batch(converting_map, settings$processing)
+    
+    fs::dir_create(fs::path(cache_folder, "processed"))
+    save(converting_process, file=fs::path(cache_folder, "processed", glue("{i}.RData")))
 
     message("\nexporting")
     target_filename <- glue("[target_folder]/[filename_out]", .open = "[", .close = "]" )
